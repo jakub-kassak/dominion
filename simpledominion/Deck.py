414e949bd9740af6936557125ae75c9c3ccea107
Jakub Kassak <mojspam.jk@gmail.com>
2021-10-20 12:45:19 +0200

    class Deck

--- simpledominion/Deck.py
+++ simpledominion/Deck.py
@@ -1,4 +1,4 @@
-from simpledominion.DiscardPile import DiscardPile
+from simpledominion.Pile import PileInterface
 from simpledominion.CardInterface import CardInterface
 from typing import List
 
@@ -9,4 +9,15 @@ class DeckInterface:
         raise NotImplementedError
 
 
+class Deck(DeckInterface):
+    def __init__(self, discardPile: PileInterface):
+        self._discardPile: PileInterface = discardPile
+        self._cards: List[CardInterface] = []
+
+    def draw(self, count: int) -> List[CardInterface]:
+        if len(self._cards) < count:
+            self._cards += self._discardPile.get_all()
+        cards: List[CardInterface] = self._cards[:count]
+        self._cards = self._cards[count:]
+        return cards
 
