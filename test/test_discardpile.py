cb5519f61639ce96aaeb17c8c082afae2afa4932
Jakub Kassak <mojspam.jk@gmail.com>
2021-10-22 21:39:07 +0200

    tests for DiscardPile

--- test/test_discardpile.py
+++ test/test_discardpile.py
@@ -1,38 +1,35 @@
+from typing import List
 from unittest import TestCase
-from simpledominion.DiscardPile import DiscardPile
-from simpledominion.GameCardType import GameCardType, GAME_CARD_TYPE_ESTATE, GAME_CARD_TYPE_COPPER
+from simpledominion.Pile import DiscardPile, PileInterface
+from simpledominion.GameCardType import *
 from simpledominion.CardInterface import CardInterface
+
 
 class FakeCard(CardInterface):
     def __init__(self, cardType: GameCardType):
         self._cardType = cardType
+
     @property
     def cardType(self):
-    	return self._cardType
+        return self._cardType
 
 
 class TestDiscardPile(TestCase):
-    def assertTopIs(self, pile, string):
-        self.assertEqual(pile.getTopCard().cardType.name, string)
-        
-    def assertTopIsNone(self, pile):
-        self.assertIsNone(pile.getTopCard())
 
     def setUp(self):
-        self.pile1 = DiscardPile([FakeCard(GAME_CARD_TYPE_ESTATE), FakeCard(GAME_CARD_TYPE_COPPER)])
-        self.pile2 = DiscardPile([])
-        
-    def test_get_top_card(self):
-        self.assertTopIs(self.pile1, "Copper")
-        self.assertTopIsNone(self.pile2)
-        
-    def test_add_cards_and_get_size(self):
-        self.assertEqual(self.pile2.getSize(), 0)
-        self.pile2.addCards([FakeCard(GAME_CARD_TYPE_ESTATE)])
-        self.assertEqual(self.pile2.getSize(), 1)
-        self.assertTopIs(self.pile2, "Estate")
-        self.pile2.addCards([FakeCard(GAME_CARD_TYPE_COPPER)])
-        self.assertEqual(self.pile2.getSize(), 2)
-        self.assertTopIs(self.pile2, "Copper")
-        
-        
+        self.discard_pile: PileInterface = DiscardPile()
+        self.discard_pile.put_into([FakeCard(GAME_CARD_TYPE_ESTATE), FakeCard(GAME_CARD_TYPE_COPPER)])
+
+    def test_put_into(self):
+        self.discard_pile.put_into([FakeCard(GAME_CARD_TYPE_ESTATE)])
+        self.assertEqual(3, len(self.discard_pile.get_all()))
+        self.discard_pile.put_into([FakeCard(GAME_CARD_TYPE_ESTATE)])
+        self.assertEqual(GAME_CARD_TYPE_ESTATE, self.discard_pile.get_all()[0].cardType)
+        cards = [FakeCard(GAME_CARD_TYPE_ESTATE), FakeCard(GAME_CARD_TYPE_COPPER),
+                 FakeCard(GAME_CARD_TYPE_FESTIVAL), FakeCard(GAME_CARD_TYPE_MARKET),
+                 FakeCard(GAME_CARD_TYPE_SMITHY), FakeCard(GAME_CARD_TYPE_LABORATORY),
+                 FakeCard(GAME_CARD_TYPE_VILLAGE), FakeCard(GAME_CARD_TYPE_DUCHY)]
+        self.discard_pile.put_into(cards)
+        shuffled_cards: List[CardInterface] = self.discard_pile.get_all()
+        self.assertEqual(len(cards), len(shuffled_cards))
+        self.assertNotEqual(cards, shuffled_cards)
