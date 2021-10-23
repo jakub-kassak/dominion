f055797850ac4af404a7bc774ef14236a06150f9
Jakub Kassak <mojspam.jk@gmail.com>
2021-10-23 10:57:18 +0200

    BuyDeckInterface

--- simpledominion/BuyDeck.py
+++ simpledominion/BuyDeck.py
@@ -4,11 +4,23 @@ from simpledominion.GameCardType import GameCardType
 from simpledominion.GameCard import GameCard
 
 
-class BuyDeck:
+class BuyDeckInterface:
+    def buy(self) -> Optional[CardInterface]:
+        raise NotImplementedError
+
+    def is_empty(self) -> bool:
+        raise NotImplementedError
+
+    @property
+    def card_type(self) -> GameCardType:
+        raise NotImplementedError
+
+
+class BuyDeck(BuyDeckInterface):
 
     def __init__(self, card_type: GameCardType, card_count: int = 10):
-        self._card_type = card_type
-        self._card_count = card_count
+        self._card_type: GameCardType = card_type
+        self._card_count: int = card_count
 
     @property
     def card_type(self) -> GameCardType:
@@ -25,3 +37,4 @@ class BuyDeck:
         if self._card_count > 0:
             self._card_count -= 1
             return GameCard(self._card_type)
+        return None
