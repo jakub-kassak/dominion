cd7c72904581b8573a28dc7502cbffd770045efa
Jakub Kassak <mojspam.jk@gmail.com>
2021-10-25 16:44:51 +0200

    tests for EndGameStrategyOr

---
+++ test/test_end_game_strategy_or.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+from unittest.mock import Mock
+from simpledominion.EndGameStrategy import EndGameStrategy, EndGameStrategyOr
+
+
+class TestNEmptyDecks(TestCase):
+    def setUp(self) -> None:
+        self.s1 = Mock()
+        self.s2 = Mock()
+        self.strategy_or: EndGameStrategy = EndGameStrategyOr(self.s1, self.s2)
+
+    def set_mocks(self, over1: bool, over2: bool):
+        self.s1.is_game_over.return_value = over1
+        self.s2.is_game_over.return_value = over2
+
+    def test_is_game_over(self):
+        self.set_mocks(False, False)
+        self.assertFalse(self.strategy_or.is_game_over())
+        self.set_mocks(True, False)
+        self.assertTrue(self.strategy_or.is_game_over())
+        self.set_mocks(False, True)
+        self.assertTrue(self.strategy_or.is_game_over())
+        self.set_mocks(True, True)
+        self.assertTrue(self.strategy_or.is_game_over())
