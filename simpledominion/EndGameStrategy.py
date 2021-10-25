737c579fb8cff46099360ef090b4dc1ed0e28321
Jakub Kassak <mojspam.jk@gmail.com>
2021-10-25 16:43:25 +0200

    class NEmptyDecks

--- simpledominion/EndGameStrategy.py
+++ simpledominion/EndGameStrategy.py
@@ -5,3 +5,16 @@ from simpledominion.BuyDeck import BuyDeckInterface
 class EndGameStrategy:
     def is_game_over(self) -> bool:
         raise NotImplementedError
+
+
+class NEmptyDecks(EndGameStrategy):
+    def __init__(self, decks: List[BuyDeckInterface], n: int):
+        self._decks = decks[:]
+        self._n = n
+
+    def is_game_over(self) -> bool:
+        count = 0
+        for deck in self._decks:
+            count += deck.is_empty()
+        return count >= self._n
+
