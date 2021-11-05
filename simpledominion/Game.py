d34e89c9173c4ce5628b244740b655920195efcc
Jakub Kassak <mojspam.jk@gmail.com>
2021-11-05 15:44:20 +0100

    Game - new properties and points evaluation

--- simpledominion/Game.py
+++ simpledominion/Game.py
@@ -27,28 +27,52 @@ class GameInterface:
     def end_turn(self) -> bool:
         raise NotImplementedError
 
-    def evaluate_points(self) -> Optional[int]:
+    @property
+    def points(self) -> int:
         raise NotImplementedError
 
     @property
     def turn_number(self) -> int:
         raise NotImplementedError
 
+    @property
+    def buy_decks(self) -> List[BuyDeckInterface]:
+        raise NotImplementedError
+
+    @property
+    def turn_status(self) -> TurnStatus:
+        raise NotImplementedError
+
 
 class Game(GameInterface):
-    def __init__(self, player: Player, buy_decks: List[BuyDeckInterface], turn_factory: TurnFactory, end_strategy: EndGameStrategy) -> None:
+    def __init__(self, player: Player, buy_decks: List[BuyDeckInterface], turn_factory: TurnFactory,
+                 end_strategy: EndGameStrategy, points: int) -> None:
         self._player: Player = player
         self._factory: TurnFactory = turn_factory
-        self._turn: TurnInterface = self._factory.new(TurnStatus(1, 1, 5, 0), self._player)
+        self._turn_status: TurnStatus = TurnStatus(1, 1, 5, 0)
+        self._turn: TurnInterface = self._factory.new(self._turn_status, self._player)
         self._buy_decks: List[BuyDeckInterface] = buy_decks
         self._end_strategy: EndGameStrategy = end_strategy
         self._turn_number: int = 1
         self._is_game_over: bool = False
         self._phase: Phase = Phase.ACTION
+        self._points: int = points
+
+    @property
+    def points(self) -> int:
+        return self._points
 
     @property
     def turn_number(self) -> int:
         return self._turn_number
+
+    @property
+    def buy_decks(self) -> List[BuyDeckInterface]:
+        return self._buy_decks[:]
+
+    @property
+    def turn_status(self) -> TurnStatus:
+        return self._turn_status
 
     def evaluate_points(self) -> Optional[int]:
         if self._is_game_over:
@@ -78,8 +102,9 @@ class Game(GameInterface):
         if self._is_game_over:
             return False
         self._phase = Phase.ACTION
-        self._turn.end_turn()
-        self._turn = self._factory.new(TurnStatus(1, 1, 5, 0), self._player)
+        self._points += self._turn.end_turn()
+        self._turn_status = TurnStatus(1, 1, 5, 0)
+        self._turn = self._factory.new(self._turn_status, self._player)
         self._is_game_over = self._end_strategy.is_game_over()
         self._turn_number += int(not self._is_game_over)
         return True
