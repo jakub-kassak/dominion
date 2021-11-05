ae37e2aaddea3c926c2febff084ae56c050091c5
Jakub Kassak <mojspam.jk@gmail.com>
2021-11-05 12:57:39 +0100

    Turn - phase check removal, new_turn removal
    Turn will not check in which phase the game is. It will only check whether it is possible to buy or play a given card. It is possible to end turn at any point and do it multiple times.
    Instead of new_turn a new Turn instance should be used.

--- simpledominion/Turn.py
+++ simpledominion/Turn.py
@@ -14,8 +14,8 @@ class Phase(IntEnum):
 
 
 class TurnInterface:
-    def new_turn(self, status: TurnStatus, player: Player) -> bool:
-        raise NotImplementedError
+    # def new_turn(self, status: TurnStatus, player: Player) -> bool:
+    #     raise NotImplementedError
 
     def play_card(self, idx: int) -> bool:
         raise NotImplementedError
@@ -23,61 +23,54 @@ class TurnInterface:
     def buy_card(self, buy_deck: BuyDeckInterface) -> bool:
         raise NotImplementedError
 
-    def end_play_card_phase(self) -> bool:
-        raise NotImplementedError
+    # def end_play_card_phase(self) -> bool:
+    #     raise NotImplementedError
 
-    def end_turn(self) -> bool:
+    def end_turn(self) -> None:
         raise NotImplementedError
 
 
 class Turn(TurnInterface):
-    def __init__(self):
-        self._status: Optional[TurnStatus] = None
-        self._player: Optional[Player] = None
-        self._phase: Phase = Phase.CLEAN_UP
+    def __init__(self, status: TurnStatus, player: Player) -> None:
+        self._status = status
+        self._player = player
+        self._player.hand.draw(status.cards)
+        status.cards = 0
 
-    def end_play_card_phase(self) -> bool:
-        if self._phase > Phase.BUY:
-            return False
-        self._phase = Phase.BUY
-        return True
+    # def end_play_card_phase(self) -> bool:
+    #     if self._phase > Phase.BUY:
+    #         return False
+    #     self._phase = Phase.BUY
+    #     return True
 
-    def end_turn(self) -> bool:
-        if self._phase == Phase.CLEAN_UP:
-            return False
-        self._phase = Phase.CLEAN_UP
-        self._player.discard_pile.put_into(self._player.play_pile.get_all())
-        self._player.discard_pile.put_into(self._player.hand.get_all())
-        return True
+    def end_turn(self) -> None:
+        self._player.discard_pile.put_into(self._player.play_pile.get_all() + self._player.hand.get_all())
 
-    def new_turn(self, status: TurnStatus, player: Player) -> bool:
-        if self._phase == Phase.CLEAN_UP:
-            self._status = status
-            self._player = player
-            self._phase = Phase.ACTION
-            self._player.hand.draw(status.cards)
-            status.cards = 0
-            return True
-        return False
+    # def new_turn(self, status: TurnStatus, player: Player) -> bool:
+    #     if self._phase == Phase.CLEAN_UP:
+    #         self._status = status
+    #         self._player = player
+    #         self._phase = Phase.ACTION
+    #         self._player.hand.draw(status.cards)
+    #         status.cards = 0
+    #         return True
+    #     return False
 
     def play_card(self, idx: int) -> bool:
-        if self._phase == Phase.ACTION and self._status.actions > 0 and self._player.hand.is_action_card(idx) \
-                or self._phase <= Phase.BUY_TREASURE and self._player.hand.is_treasure_card(idx):
-            if self._player.hand.is_treasure_card(idx):
-                self._phase = Phase.BUY_TREASURE
-            if self._phase == Phase.ACTION:
-                self._status.actions -= 1
-            card: CardInterface = self._player.hand.play(idx)
-            card.evaluate(self._status)
-            self._player.play_pile.put_into([card])
-            self._player.hand.draw(self._status.cards)
-            return True
+        if self._status.actions > 0 and self._player.hand.is_action_card(idx) \
+                or self._player.hand.is_treasure_card(idx):
+            card: Optional[CardInterface] = self._player.hand.play(idx)
+            if card:
+                if card.cardType.isAction:
+                    self._status.actions -= 1
+                card.evaluate(self._status)
+                self._player.play_pile.put_into([card])
+                self._player.hand.draw(self._status.cards)
+                return True
         return False
 
     def buy_card(self, buy_deck: BuyDeckInterface) -> bool:
-        if self._phase == Phase.BUY \
-                and self._status.coins >= buy_deck.card_type.cost \
-                and self._status.buys > 0:
+        if self._status.coins >= buy_deck.card_type.cost and self._status.buys > 0:
             card: Optional[CardInterface] = buy_deck.buy()
             if card:
                 self._status.buys -= 1
