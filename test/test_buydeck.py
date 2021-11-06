65fe4a91d0b6e13af6a62d05c824b7c75a5dd986
Jakub Kassak <mojspam.jk@gmail.com>
2021-11-06 10:51:45 +0100

    test_buydeck - code simplification

--- test/test_buydeck.py
+++ test/test_buydeck.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from simpledominion.BuyDeck import BuyDeck
+from simpledominion.BuyDeck import BuyDeck, BuyDeckInterface
 from simpledominion.CardInterface import CardInterface
 from simpledominion.GameCardType import GAME_CARD_TYPE_COPPER, GAME_CARD_TYPE_FESTIVAL
 
@@ -7,19 +7,19 @@ from simpledominion.GameCardType import GAME_CARD_TYPE_COPPER, GAME_CARD_TYPE_FE
 class TestBuyDeck(TestCase):
 
     def setUp(self) -> None:
-        self.buy_deck1 = BuyDeck(GAME_CARD_TYPE_COPPER)
-        self.deck2_size = 5
-        self.buy_deck2 = BuyDeck(GAME_CARD_TYPE_FESTIVAL, self.deck2_size)
+        self.buy_deck1: BuyDeckInterface = BuyDeck(GAME_CARD_TYPE_COPPER)
+        self.deck2_size: int = 5
+        self.buy_deck2: BuyDeckInterface = BuyDeck(GAME_CARD_TYPE_FESTIVAL, self.deck2_size)
 
-    def test_buy_is_empty(self):
+    def test_buy_is_empty(self) -> None:
         self.assertEqual(GAME_CARD_TYPE_COPPER, self.buy_deck1.card_type)
         for i in range(9):
             self.assertEqual(10 - i, self.buy_deck1.size)
             self.assertIsInstance(self.buy_deck1.buy(), CardInterface)
-        self.assertEqual(False, self.buy_deck1.is_empty())
+        self.assertFalse(self.buy_deck1.is_empty())
         self.buy_deck1.buy()
-        self.assertEqual(True, self.buy_deck1.is_empty())
+        self.assertTrue(self.buy_deck1.is_empty())
         for i in range(self.deck2_size):
             self.assertEqual(GAME_CARD_TYPE_FESTIVAL, self.buy_deck2.buy().cardType)
-        self.assertEqual(None, self.buy_deck1.buy())
-        self.assertEqual(None, self.buy_deck2.buy())
+        self.assertIsNone(self.buy_deck1.buy())
+        self.assertIsNone(self.buy_deck2.buy())
