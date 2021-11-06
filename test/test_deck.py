78c63ef0a2d0cab0f22486cb9c826efff8290492
Jakub Kassak <mojspam.jk@gmail.com>
2021-11-06 10:56:12 +0100

    test_deck - removal of unnecessary code

--- test/test_deck.py
+++ test/test_deck.py
@@ -8,24 +8,18 @@ from typing import List
 
 
 class TestDeck(TestCase):
-
-    def assert_has_same_cards(self, list1: List[GameCardType], list2: List[GameCardType]):
-        self.assertEqual(len(list1), len(list2))
-        for i in range(len(list1)):
-            self.assertEqual(list1[i], list2[i])
-
     def setUp(self) -> None:
-        self.dp = Mock()
+        self.dp: Mock = Mock()
         self.deck: DeckInterface = Deck(self.dp)
 
-    def assert_calls_type_len(self, ret_val: List[CardInterface], card_type: GameCardType, calls: int, count: int):
+    def assert_calls_type_len(self, ret_val: List[CardInterface], card_type: GameCardType, calls: int, count: int) -> None:
         self.assertEqual(count, len(ret_val))
         self.assertEqual(calls, self.dp.get_all.call_count)
         self.dp.get_all.call_count = 0
         if count > 0:
             self.assertEqual(card_type, ret_val[-1].cardType)
 
-    def test_draw(self):
+    def test_draw(self) -> None:
         self.dp.get_all.return_value = []
         self.assert_calls_type_len(self.deck.draw(5), GAME_CARD_TYPE_ESTATE, 1, 0)
         self.dp.get_all.return_value = [GameCard(GAME_CARD_TYPE_ESTATE)] * 3 + [GameCard(GAME_CARD_TYPE_COPPER)] * 7
