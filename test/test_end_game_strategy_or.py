81a693213e9bcc664bfde24a92851790c322a7df
Jakub Kassak <mojspam.jk@gmail.com>
2021-11-06 10:58:08 +0100

    test_end_game_strategy_or - type hints

--- test/test_end_game_strategy_or.py
+++ test/test_end_game_strategy_or.py
@@ -5,15 +5,15 @@ from simpledominion.EndGameStrategy import EndGameStrategy, EndGameStrategyOr
 
 class TestNEmptyDecks(TestCase):
     def setUp(self) -> None:
-        self.s1 = Mock()
-        self.s2 = Mock()
+        self.s1: Mock = Mock()
+        self.s2: Mock = Mock()
         self.strategy_or: EndGameStrategy = EndGameStrategyOr(self.s1, self.s2)
 
-    def set_mocks(self, over1: bool, over2: bool):
+    def set_mocks(self, over1: bool, over2: bool) -> None:
         self.s1.is_game_over.return_value = over1
         self.s2.is_game_over.return_value = over2
 
-    def test_is_game_over(self):
+    def test_is_game_over(self) -> None:
         self.set_mocks(False, False)
         self.assertFalse(self.strategy_or.is_game_over())
         self.set_mocks(True, False)
